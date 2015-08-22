27d0f57025dd2a1749ed013b65438fc0d21a289e
Bahtiyar Biksayev <biksayev@mc2soft.ru>
2015-08-22 04:01:10 +0300

    Add nested templates execution

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+
+	"github.com/bikbah/lyclife/mtemplate"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	templFuncMap = make(template.FuncMap)
+)
+
+func main() {
+	router := gin.Default()
+
+	router.HTMLRender = createMyRender()
+	router.Static("/static", "./static")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "base", nil)
+	})
+	router.Run(":8888")
+}
+
+func createMyRender() mtemplate.Render {
+
+	templFuncMap["static"] = func() template.HTML {
+		return template.HTML(fmt.Sprint("static/"))
+	}
+	templFuncMap["news_load"] = func() template.HTML {
+		return template.HTML(fmt.Sprint("news_load"))
+	}
+
+	templFuncMap["topmenu"] = func() template.HTML {
+		return template.HTML(fmt.Sprint(`topmenu`))
+	}
+
+	r := mtemplate.New()
+	//r.AddFromFiles("index", "templates/base.html", "templates/layout.html", "templates/index.html")
+
+	//t2 := template.Must(template.ParseFiles("templates/layout.html", "templates/base.html"))
+	//t2 = t2.Funcs(templFuncMap)
+	bytesBase, err := ioutil.ReadFile("templates/base.html")
+	check(err)
+	bytesLayout, err := ioutil.ReadFile("templates/layout.html")
+	check(err)
+	bytesPage, err := ioutil.ReadFile("templates/index.html")
+	check(err)
+
+	t := template.New("base")
+	t = t.Funcs(templFuncMap)
+	t, err = t.Parse(string(bytesBase))
+	check(err)
+
+	t, _ = t.Clone()
+	t = t.New("layout")
+	t, err = t.Parse(string(bytesLayout))
+	check(err)
+
+	t, _ = t.Clone()
+	t = t.New("index")
+	t, err = t.Parse(string(bytesPage))
+	check(err)
+
+	r.Add("base", t)
+
+	return r
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
